f6eddc7a6cc486b1b9bcfb2196d19ecc86b6950f
Krystal Foster <kmichelefoster@gmail.com>
2018-10-12 13:11:21 -0700

    map through data array to return first ten queried results

--- src/main.js
+++ src/main.js
@@ -42,26 +42,38 @@ $(document).ready(function() {
         format: 'json'
       },
       success: function(response) {
+        console.log(response.data);
+        response.data.map((doctor,index) => {
+          if(index <= 9){
+            let phoneNumber = doctor.practices[0].phones[0].number;
+            let newPatients = doctor.practices[0].accepts_new_patients;
+            let picture = doctor.profile.image_url;
+            let website = doctor.practices[0].website;
+            let fullName = doctor.profile.first_name + " " + doctor.profile.last_name;
+            $('.doctor-list').append(
+              "<br><img src='" + picture + "'></br>" +
+              "<br><p>First Name: </p>" + fullName +
+              "<br><p>Phone number: </p>" + phoneNumber +
+              "<br><p>Accepting new patients: " + newPatients +
+              "<br><p>Website: "+ website + "."
+            );
+            }
+          });
 
-        let phoneNumber = response.data[0].practices[0].phones[0].number;
-        let newPatients = response.data[0].practices[0].accepts_new_patients;
-        let picture = response.data[0].profile.image_url;
-        let website = response.data[0].practices[0].website;
-        let fullName = response.data[0].profile.first_name + " " + response.data[0].profile.last_name;
+
+        // let phoneNumber = response.data[0].practices[0].phones[0].number;
+        // let newPatients = response.data[0].practices[0].accepts_new_patients;
+        // let picture = response.data[0].profile.image_url;
+        // let website = response.data[0].practices[0].website;
+        // let fullName = response.data[0].profile.first_name + " " + response.data[0].profile.last_name;
+
       // }
-        // $('.doctor-list').append(
-        //   "<br><img src='" + picture + "'></br>" +
-        //   "<br><p>First Name: </p>" + fullName +
-        //   "<br><p>Phone number: </p>" + phoneNumber +
-        //   "<br><p>Accepting new patients: " + newPatients +
-        //   "<br><p>Website: "+ website + "."
-        // );
 
-        $('.name-info').text(fullName);
-        $('.phone-number').text(phoneNumber);
-        $('.website').text(website);
-        $('.newPatients').text("Accepting new patients: " + newPatients);
-        $('.doc-image').html("<img src='" + picture + "'>");
+        // $('.name-info').text(fullName);
+        // $('.phone-number').text(phoneNumber);
+        // $('.website').text(website);
+        // $('.newPatients').text("Accepting new patients: " + newPatients);
+        // $('.doc-image').html("<img src='" + picture + "'>");
       },
       error: function() {
         $('#errors').append("There was an error processing your request. Please try again.");
