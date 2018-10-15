22602ec3e0afc315a6f0a44587cb79cab1c78a5f
Krystal Foster <kmichelefoster@gmail.com>
2018-10-14 18:01:20 -0700

    adjust proper address endpoints

--- src/error.js
+++ src/error.js
@@ -2,7 +2,7 @@ export class DoctorSearch {
   getDoctorBySearch(search) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
       let search = $('#search-input').val();
       $('#search-input').val('');
       $.ajax({
-        url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
+        url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`,
         type: 'GET',
         data: {
           format: 'json'
@@ -23,13 +23,13 @@ $(document).ready(function() {
             $('.errors').text('Sorry, there are currently no doctors in Portland that match that search.');
           } else {
             response.data.map((doctor,index) => {
-              if(index <= 9){
+              if(index <= 30){
                 let phoneNumber = doctor.practices[0].phones[0].number;
                 let newPatients = doctor.practices[0].accepts_new_patients;
                 let picture = doctor.profile.image_url;
                 let website = doctor.practices[0].website;
                 let fullName = doctor.profile.first_name + " " + doctor.profile.last_name;
-                let address = response.data[0].practices[0].visit_address.street  +  ",<br> " +  response.data[0].practices[0].visit_address.city + ", " + response.data[0].practices[0].visit_address.state + " <br>" +  response.data[0].practices[0].visit_address.zip;
+                let address = doctor.practices[0].visit_address.street  +  ",<br> " +  doctor.practices[0].visit_address.city + ", " + doctor.practices[0].visit_address.state + ", " +  doctor.practices[0].visit_address.zip;
                 $('.doctor-list').append(
                   "<div><img src='" + picture + "'>" +
                   "<p><strong>Name:</strong> " + fullName + "</p>" +
